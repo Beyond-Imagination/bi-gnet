f4e16be74d43a555156fb026c92d15c4fd23ae49
Kyung <xoelxowl92@gmail.com>
2022-06-25 00:18:40 +0900

    약관동의 페이지 Front 완성

--- src/terms/TermContainer.tsx
+++ src/terms/TermContainer.tsx
@@ -4,11 +4,17 @@ import TermView from "./TermView";
 interface TermContainerProps {
 
 }
-
+//생년월일 : year, month, day
 const TermContainer: React.FC<TermContainerProps> = ({}) => {
+    const [year,setYear] = React.useState<number>(0);
+    const [month,setMonth] = React.useState<number>(0);
+    const [day,setDay] = React.useState<number>(0);
+    const [gender,setGender] = React.useState<number>(0);
+    const [terms,setTerms] = React.useState<boolean[]>([false,false]);
+
     return (
         <main style={{padding: "1rem 0"}}>
-            <TermView/>
+            <TermView year={year} month={month} day={day} gender={gender} terms={terms} setYear={setYear} setMonth={setMonth} setDay={setDay} setGender={setGender} setTerms={setTerms}/>
         </main>
     );
 }
