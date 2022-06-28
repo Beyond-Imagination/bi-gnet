90fa174ba9adc6c0648bf6f055a23e12f1b283d3
laggu <laggu91@gmail.com>
2022-06-28 22:21:02 +0900

    use province for getting benefit

--- src/benefit/BenefitContainer.tsx
+++ src/benefit/BenefitContainer.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import BenefitView from "./BenefitView";
 import InfiniteScroll from 'react-infinite-scroller';
 import {IBenefit} from "./Benefit";
+import {useRecoilState} from "recoil";
+import {provinceState} from "../global-state/province";
 
 interface BenefitContainerProps {
 
@@ -14,9 +16,10 @@ interface BenefitState {
 
 const BenefitContainer: React.FC<BenefitContainerProps> = ({}) => {
     const [state, setState] = React.useState<BenefitState>({benefits: [], hasMore: true});
+    const [province, setProvince] = useRecoilState(provinceState);
 
     const loadFunc = (page:number) => {
-        fetch(`http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/benefits?page=${page}`) // host 를 환경변수에서 읽도록 수정
+        fetch(`http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/benefits?page=${page}&province=${province}`) // host 를 환경변수에서 읽도록 수정
             .then((response) => response.json())
             .then((data) => {
                 let clone = {...state};
