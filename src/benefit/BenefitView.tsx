4a3f5ef0ab1761deb4637c4a3e67a707c7db058b
Kuwon Sebastian Ra <laggu91@gmail.com>
2022-06-29 00:34:05 +0900

    add link for benefit page from home (#9)

--- src/benefit/BenefitView.tsx
+++ src/benefit/BenefitView.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Benefit, {IBenefit} from "./Benefit";
+import Typography from "@mui/material/Typography";
+import {useNavigate} from "react-router-dom";
 
 interface BenefitViewProps {
+    province: string
     benefits: IBenefit[]
 }
 
-const BenefitView: React.FC<BenefitViewProps> = ({benefits}) => {
+const BenefitView: React.FC<BenefitViewProps> = ({province, benefits}) => {
+    const navigation = useNavigate();
     return (
         <>
-            <h2>귀농 혜택</h2>
+            {
+                window.location.href.endsWith("benefit") ?
+                <h2>귀농 혜택</h2> :
+                <div style={{marginBottom: 12}} onClick={() => {
+                    navigation('/benefit')
+                }}>
+                    <Typography variant={'subtitle1'}>{`${province} 귀농혜택 > `}</Typography>
+                </div>
+            }
             {
                 benefits.map(benefit => <Benefit key={benefit._id} data={benefit} />)
             }
